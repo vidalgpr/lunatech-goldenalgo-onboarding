1f59faba386801bf15c3a7cde2a920a77670a15b
Vidal Gonzalez-Pereira <vidal.pereira@lunatech.be>
2021-10-04 18:35:19 +0200

    feat [Main]: elastic connector intialisation

--- server/src/main/scala/com/lunatech/goldenalgo/onboarding/Main.scala
+++ server/src/main/scala/com/lunatech/goldenalgo/onboarding/Main.scala
@@ -6,13 +6,20 @@ package com.lunatech.goldenalgo.onboarding
 
 import akka.actor.ActorSystem
 import scala.concurrent.ExecutionContext
+import com.lunatech.goldenalgo.onboarding.adapter.DBConnector
 
 object Main {
 
   def main(args: Array[String]) = {
-    implicit val system: ActorSystem  = ActorSystem("main-system")
-    implicit val ec: ExecutionContext = system.dispatcher
 
+    val env = sys.env
+    val dbHost = env.get("DB_HOST")
+    val dbIndex = env.get("DB_INDEX") //required
+
+    implicit val system: ActorSystem = ActorSystem("main-system")
+    implicit val ec: ExecutionContext = system.dispatcher
+    
+    implicit val dbClient = new DBConnector(dbHost, dbIndex)
     val controller = new Controller()
 
     new WebServer(controller)

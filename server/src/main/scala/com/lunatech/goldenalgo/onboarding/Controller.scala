1910cdbb6ab201439bbb2666c03743f9a57a1401
Vidal Gonzalez-Pereira <vidal.pereira@lunatech.be>
2021-10-05 19:01:47 +0200

    refactor / feat [Controller]: refactor sub-routes and add deserialization step in postRecipeRoute

--- server/src/main/scala/com/lunatech/goldenalgo/onboarding/Controller.scala
+++ server/src/main/scala/com/lunatech/goldenalgo/onboarding/Controller.scala
@@ -1,53 +1,82 @@
 package com.lunatech.goldenalgo.onboarding
 
+import akka.actor.ActorSystem
+import akka.stream.Materializer
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.model._
 import io.circe.syntax._
+import io.circe.parser.decode
 import com.sksamuel.elastic4s.circe._
+import com.sksamuel.elastic4s.{RequestSuccess, RequestFailure}
 import com.lunatech.goldenalgo.onboarding.adapter.DBConnector
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 import scala.util.{Success, Failure}
 
 class Controller()(
     implicit val ec: ExecutionContext,
-    implicit val dbClient: DBConnector
+    implicit val db: DBConnector,
+    implicit val system: ActorSystem,
+    implicit val matz: Materializer
 ) {
 
-  // TODO: replace this hard-coded value by CRUD operations
-  val recipe1 = Recipe(
-    "recipeyyyyyy1-id",
-    "recipeyyyyyy1-name",
-    Seq("ingredient1", "ingredient2"),
-    Seq("instruction1", "instruction2")
-  )
+  def toHttpEntity(payload: String) =
+    HttpEntity(ContentTypes.`application/json`, payload)
 
-  val recipes = path("recipes") {
+  db.initIdx()
+
+  val getRecipeRoute: Route = {
     parameter(Symbol("id")) { (recipeId: String) =>
       get {
-        val response = dbClient.queryIdx(recipeId)
-        val result = response.map(_.result.to[Recipe])
-
-        onComplete(result) {
-          case Success(r) if r.length == 0 => complete(StatusCodes.NotFound)
-          case Success(r) =>
-            complete(
-              HttpEntity(ContentTypes.`application/json`, r.asJson.noSpaces)
-            )
-          case Failure(e) => complete(StatusCodes.BadRequest, e.getMessage)
-        }
+        complete(
+          db.matchQueryIdx("id", recipeId)
+            .map(_.result.to[Recipe])
+            .map(_.asJson.noSpaces)
+            .map(toHttpEntity)
+        )
+      }
+    } ~ parameter(Symbol("name")) { (recipeName: String) =>
+      get {
+        complete(
+          db.matchQueryIdx("name", recipeName)
+            .map(_.result.to[Recipe])
+            .map(_.asJson.noSpaces)
+            .map(toHttpEntity)
+        )
       }
     }
   }
 
-  val postRecipe = path("recipe" / "test_upload") {
-    get {
-      dbClient.initIdx()
-      dbClient.idxInto(recipe1)
+  lazy val postRecipeRoute = {
+    (post & pathEndOrSingleSlash & extractRequest & extractLog) {
+      (request, log) =>
+        val entity = request.entity
+        val strictEntity = entity.toStrict(2.seconds)
+        val recipe = strictEntity
+          .map(_.data.utf8String)
+          .map(decode[Recipe](_))
 
-      complete(StatusCodes.OK)
+        onComplete(recipe) {
+          _.flatMap(_.toTry) match {
+            case Success(recipe) =>
+              log.info(s"Got recipe: $recipe")
+
+              complete(
+                db.idxInto(recipe).map {
+                  case RequestSuccess(_, _, _, _) => StatusCodes.OK
+                  case RequestFailure(_, _, _, _) => StatusCodes.InternalServerError
+                }
+              )
+
+            case Failure(ex) => failWith(ex)
+          }
+        }
     }
   }
 
-  val routes: Route = recipes ~ postRecipe
+  lazy val routes: Route = concat(
+    path("api" / "recipes")(getRecipeRoute),
+    path("recipe" / "test_upload")(postRecipeRoute)
+  )
 }

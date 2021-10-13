733a5b559fb6b4d3952c115409e452710a05702d
Vidal Gonzalez-Pereira <vidal.pereira@lunatech.be>
2021-10-13 12:02:12 +0200

    feat [BackendService]: add postRecipe method

--- client/src/main/scala/com/lunatech/goldenalgo/onboarding/BackendService.scala
+++ client/src/main/scala/com/lunatech/goldenalgo/onboarding/BackendService.scala
@@ -1,23 +1,22 @@
 package com.lunatech.goldenalgo.onboarding
 
 import org.scalajs.dom.ext.Ajax
-import io.circe.parser._
+import io.circe.parser._, io.circe.syntax._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Failure, Success }
-import japgolly.scalajs.react.Callback
-import japgolly.scalajs.react.CallbackTo
-import diode.{ Dispatcher, Effect, ModelRO }
-import scala.concurrent.Future
+import diode.Effect
 import diode.EffectSingle
 
 object BackendService {
+
+  val base = "http://127.0.0.1:8080"
 
   def fetchRecipes(): Unit = Ajax.get("/recipes").onComplete {
     case Success(xhr) => decode[List[Recipe]](xhr.responseText)
     case Failure(t)   => println("An error has occurred: " + t.getMessage)
   }
 
-  def fetchRecipeByIdEffect(id: String) = Effect(Ajax.get(s"http://127.0.0.1:8080/api/search_recipes/all").map { xhr =>
+  def fetchRecipeByIdEffect(id: String) = Effect(Ajax.get(s"$base/api/recipe/get?id=$id").map { xhr =>
     decode[Seq[Recipe]](xhr.responseText).fold(
       err => ResetRecipe,
       _ match {
@@ -27,6 +26,16 @@ object BackendService {
     )
   })
 
-  // def postRecipe(recipe: Recipe): Unit = Effect(Ajax.post(s"/api/upload_recipe?id=$user").map(r => NewMessages(r.responseText)))
+  def postRecipe(recipe: Recipe) = Effect(
+    Ajax
+      .post(
+        s"$base/api/recipe/post",
+        data = Ajax.InputData.str2ajax(recipe.asJson.noSpaces),
+        headers = Map("Content-Type" -> "application/json")
+      )
+      .collect {
+        case xhr if xhr.status == 200 => ResetRecipe
+      }
+  )
 
 }

bf5ec817c94fa6b9cc679fd5f72526460299b12d
Vidal Gonzalez-Pereira <vidal.pereira@lunatech.be>
2021-10-13 12:02:59 +0200

    feat [Home]: add recipe retrieval section

--- client/src/main/scala/com/lunatech/goldenalgo/onboarding/Home.scala
+++ client/src/main/scala/com/lunatech/goldenalgo/onboarding/Home.scala
@@ -17,7 +17,7 @@ object Home {
       ctl: RouterCtl[AppRouter.Page]
   )
 
-  case class State(editRecipe: Recipe)
+  case class State(editRecipe: Recipe, editId: String)
 
   class Backend($ : BackendScope[Props, State]) {
     def mounted(props: Props) = Callback {}
@@ -25,10 +25,22 @@ object Home {
     def onSubmitButtonPressed(dispatch: Action => Callback)(currentRecipe: Recipe): Option[Callback] =
       if (currentRecipe.name.trim.nonEmpty && currentRecipe.ingredients.nonEmpty)
         Some(
-          dispatch(SetRecipe(currentRecipe)) >>
+          dispatch(SetNewRecipe(currentRecipe)) >>
+          dispatch(PostRecipe) >>
           Callback.alert("Succefully received recipe!")
         )
       else None
+
+    def onSearchByIdButtonPressed(dispatch: Action => Callback)(currentId: String): Option[Callback] =
+      if (currentId.trim.nonEmpty)
+        Some(
+          dispatch(LoadRecipe(currentId)) >>
+          Callback.info("Showing found recipes!")
+        )
+      else None
+
+    val editIdFieldChanged: ReactEventFromInput => Callback = e =>
+      Callback(e.persist()) >> $.modState(s => s.copy(editId = e.target.value))
 
     def editFieldChanged(field: RecipeField): ReactEventFromInput => Callback = e =>
       Callback(e.persist()) >> $.modState { s =>
@@ -44,44 +56,69 @@ object Home {
       val dispatch: Action => Callback = p.proxy.dispatchCB
 
       <.div(
-        ^.padding         := "250px",
-        ^.textAlign       := "center",
-        ^.backgroundColor := "#d5aca3",
-        <.h2("My Awesome Recipe"),
-        <.br,
+        <.h1("My Awesome Recipe"),
+        ^.textAlign := "center",
         <.p(
-          "Current Circuit Recipe = ",
+          "Current Circuit Recipe",
+          <.br,
           <.b(proxy.map(_.toString))
         ),
-        <.p(
-          "Current State Recipe = ",
-          <.b(s.editRecipe.toString)
+        <.div(
+          ^.padding         := "32px",
+          ^.textAlign       := "center",
+          ^.backgroundColor := "#cfd5a9",
+          <.h3("Upload your recipe here"),
+          <.p(
+            "Current Recipe State = ",
+            <.b(s.editRecipe.toString)
+          ),
+          <.input.text(
+            ^.onChange ==> editFieldChanged(RecipeName),
+            ^.placeholder := "Pizza a la leña",
+            ^.autoFocus   := true,
+            ^.size        := 50
+          ),
+          <.br,
+          <.textarea(
+            ^.onChange ==> editFieldChanged(RecipeIngredients),
+            ^.placeholder := "Harina\nTomates\n..."
+          ),
+          <.br,
+          <.textarea(
+            ^.onChange ==> editFieldChanged(RecipeInstructions),
+            ^.placeholder := "1)...\n2)...\n3)..."
+          ),
+          <.br,
+          <.button(
+            "SUBMIT",
+            ^.onClick -->? onSubmitButtonPressed(dispatch)(s.editRecipe)
+          )
         ),
-        <.input.text(
-          ^.onChange ==> editFieldChanged(RecipeName),
-          ^.placeholder := "Pizza a la leña",
-          ^.autoFocus   := true,
-          ^.size        := 50
-        ),
-        <.br,
-        <.textarea(
-          ^.onChange ==> editFieldChanged(RecipeIngredients),
-          ^.placeholder := "Harina\nTomates\n..."
-        ),
-        <.br,
-        <.textarea(
-          ^.onChange ==> editFieldChanged(RecipeInstructions),
-          ^.placeholder := "1)...\n2)...\n3)..."
-        ),
-        <.br,
-        <.button(
-          "SUBMIT",
-          ^.onClick -->? onSubmitButtonPressed(dispatch)(s.editRecipe)
-        ),
-        <.br,
-        <.button(
-          "GET RECIPE ID 00",
-          ^.onClick --> dispatch(LoadRecipe("00"))
+        <.div(
+          ^.padding         := "32px",
+          ^.textAlign       := "center",
+          ^.backgroundColor := "#c2d25e",
+          <.br,
+          <.h3("Retrieve recipes here:"),
+          <.p(
+            "Current ID State = ",
+            <.b(s.editId)
+          ),
+          <.h5("Search by ID:"),
+          <.input.text(
+            ^.onChange ==> editIdFieldChanged,
+            ^.placeholder := "my-recipe-id",
+            ^.size        := 50
+          ),
+          <.button(
+            "GET RECIPE",
+            ^.onClick -->? onSearchByIdButtonPressed(dispatch)(s.editId)
+          ),
+          <.br,
+          <.button(
+            "SHOW ALL",
+            ^.onClick -->? onSearchByIdButtonPressed(dispatch)(s.editId)
+          ),
         )
       )
     }
@@ -90,7 +127,7 @@ object Home {
   def component =
     ScalaComponent
       .builder[Props]("Home")
-      .initialStateFromProps(props => State(Recipe("", "", Nil, Nil)))
+      .initialStateFromProps(props => State(Recipe("", "", Nil, Nil), "-1"))
       .renderBackend[Backend]
       .build
 
